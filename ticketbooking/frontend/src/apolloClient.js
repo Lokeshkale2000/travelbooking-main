6441a0187da5fdb6c1eb5aff827c1055335cfb3d
Lokeshkale2000 <lokesh.21910292@viit.ac.in>
2024-10-09 09:53:05 +0530

    last update

--- ticketbooking/frontend/src/apolloClient.js
+++ ticketbooking/frontend/src/apolloClient.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql', 
+  uri: 'https://travelbooking-main-2lq1.vercel.app/graphql', 
   cache: new InMemoryCache(),
 });
 
